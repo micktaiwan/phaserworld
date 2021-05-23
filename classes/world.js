c406550b587069d74b46be4638256bd9d8b35c9f
MickaelFM <faivrem@gmail.com>
2021-05-23 16:47:57 +0200

    minor

--- classes/world.js
+++ classes/world.js
@@ -25,9 +25,16 @@ export class World {
 }
 
 class Scene extends Phaser.Scene {
+
   constructor() {
     super();
-    this.speed = 200;
+    this.zoom = 2;
+    this.setSpeed(200);
+  }
+
+  setSpeed(speed) {
+    this.speed = speed;
+    this.frameRate = Math.ceil(this.speed / 20);
   }
 
   preload() {
@@ -35,51 +42,62 @@ class Scene extends Phaser.Scene {
   }
 
   create() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.mouse = this.input.mouse;
+    console.log(this.cursors, this.mouse);
+
     this.cameras.main.setBounds(0, 0, 1024, 2048);
-    this.cameras.main.setZoom(2);
+    this.cameras.main.setZoom(this.zoom);
     this.cameras.main.centerOn(0, 0);
-    this.input.on('pointerdown', () => {
-      console.log('test');
-      this.cameras.main.centerOn(100, 100);
+
+    this.input.on('pointerdown', function() {
+      // this.cameras.main.centerOn(100, 100);
+      const diffX = this.mouse.manager.activePointer.position.x / this.zoom - this.player.body.position.x;
+      const diffY = this.mouse.manager.activePointer.position.y / this.zoom - this.player.body.position.y;
+      // console.log(this.player.body.position.x, this.mouse.manager.activePointer.position.x, diffX);
+      this.player.setVelocityX(diffX * 20);
+      this.player.setVelocityY(diffY * 20);
     }, this);
+
     this.player = this.physics.add.sprite(384, 32, 'player');
-    this.player.setBounce(0.2);
+    // this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
+
     this.anims.create({
       key: 'right',
       frames: this.anims.generateFrameNumbers('player', {start: 0, end: 5}),
-      frameRate: 10,
+      frameRate: this.frameRate,
       repeat: -1,
     });
     this.anims.create({
       key: 'up',
       frames: this.anims.generateFrameNumbers('player', {start: 6, end: 11}),
-      frameRate: 10,
+      frameRate: this.frameRate,
       repeat: -1,
     });
     this.anims.create({
       key: 'left',
       frames: this.anims.generateFrameNumbers('player', {start: 12, end: 17}),
-      frameRate: 10,
+      frameRate: this.frameRate,
       repeat: -1,
     });
     this.anims.create({
       key: 'down',
       frames: this.anims.generateFrameNumbers('player', {start: 18, end: 23}),
-      frameRate: 10,
+      frameRate: this.frameRate,
       repeat: -1,
     });
-    this.anims.create({
-      key: 'still',
-      frames: [{key: 'player', frame: 18}],
-      frameRate: 20,
-    });
+    // this.anims.create({
+    //   key: 'still',
+    //   frames: [{key: 'player', frame: 18}],
+    //   frameRate: 0,
+    // });
 
-    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
-    // console.log(this.cursors);
+    if(this.cursors.shift.isDown) this.setSpeed(300);
+    else this.setSpeed(100);
     if(this.cursors.left.isDown) {
       this.player.setVelocityX(-this.speed);
       this.player.setVelocityY(0);
